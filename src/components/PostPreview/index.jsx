51d6272d38279f85c9f7e5aa1d8e5397edea2195
Ole Martin Ruud <barskern@outlook.com>
2018-02-06 00:02:13 +0100

    Update `PostPreview` to not include a author

--- src/components/PostPreview/index.jsx
+++ src/components/PostPreview/index.jsx
@@ -3,13 +3,7 @@ import PropTypes from 'prop-types'
 import { navigateTo } from 'gatsby-link'
 import moment from 'moment'
 
-import { Card, Avatar, Tooltip, Tag } from 'antd'
-
-import { authorInitialsFromName } from '../../utils/author'
-import {
-  hexColorFromText,
-  namedColorFromText
-} from '../../utils/general'
+import { Card, Tag } from 'antd'
 
 const formatPreviewDate = (dateString) => {
   const date = moment(dateString)
@@ -17,57 +11,29 @@ const formatPreviewDate = (dateString) => {
     .isAfter(moment().month(-1)) ? date.fromNow() : date.format('D. MMMM, YYYY')
 }
 
-const makeTags = (tags) => {
-  return tags.map(
-    tag =>
-      <Tag
-        key={tag}
-        color={namedColorFromText(tag)}>
-        {tag}
-      </Tag>
-  )
+const style = {
+  width: '95%',
+  margin: '10px auto'
 }
 
-const PostPreview = (props) => {
-  const { title, date, author, path, excerpt, tags } = props
-
-  const authorInitials = authorInitialsFromName(author)
-  const authorAvatarURL = ''
-  const avatarColor = hexColorFromText(author)
-
-  const authorAvatar = (
-    <Tooltip title={author} placement="bottomRight">
-      <Avatar
-        shape="square"
-        size="large"
-        style={{ backgroundColor: avatarColor }}
-        src={authorAvatarURL}>
-        {authorInitials}
-      </Avatar>
-    </Tooltip>
-  )
-  return (
-    <Card
-      title={title}
-      hoverable
-      onClick={() => navigateTo(path)}
-      extra={formatPreviewDate(date)}
-      style={{ width: '95%', margin: '10px auto' }}
-      actions={[makeTags(tags)]}>
-      <Card.Meta
-        avatar={authorAvatar}
-        description={excerpt}/>
-    </Card>
-  )
-}
+const PostPreview = ({ title, date, path, excerpt, tags }) =>
+  <Card
+    title={title}
+    hoverable
+    onClick={() => navigateTo(path)}
+    extra={formatPreviewDate(date)}
+    style={style}
+    actions={[tags.map(tagName => <Tag key={tagName}>{tagName}</Tag>)]}>
+    <Card.Meta
+      description={excerpt}/>
+  </Card>
 
 export default PostPreview
 
 PostPreview.propTypes = {
   title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   excerpt: PropTypes.string.isRequired,
-  tags: PropTypes.array
+  tags: PropTypes.arrayOf(PropTypes.string)
 }
