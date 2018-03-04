53582bee51bc0cbf5fc0b60752ee88e98a262065
Ole Martin Ruud <barskern@outlook.com>
2018-03-04 23:11:48 +0100

    Move location of project data

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/data/json`,
+        path: `${__dirname}/src/data`,
         name: 'json'
       }
     },
